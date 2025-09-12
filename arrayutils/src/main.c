0af1ede8cb1f5572bc0f044f79f6d2f21c16cb0e
umarnurmatov <umarkhonnur12@gmail.com>
2025-09-13 01:36:28 +0300

    #FIXED rhombarray

--- arrayutils/src/main.c
+++ arrayutils/src/main.c
@@ -1,18 +1,26 @@
 #include "trianglearray.h"
+#include "rhombarray.h"
 #include "logutils.h"
 
 int main()
 {
     utils_init_log("log.txt", "log");
 
-    triangle_array_t arr; 
-    utils_triangle_array_init(&arr, 5);
+    triangle_array_t tarr; 
+    utils_triangle_array_init(&tarr, 5);
     
-    utils_triangle_array_set(&arr, 5, 1, 0);
+    utils_triangle_array_set(&tarr, 5, 1, 0);
 
-    utils_triangle_array_print(&arr);
+    utils_triangle_array_print(&tarr);
 
-    utils_triangle_array_free(&arr);
+    utils_triangle_array_free(&tarr);
+
+    rhomb_array_t rarr;
+    utils_rhomb_array_init(&rarr, 5);
+    
+    utils_rhomb_array_print(&rarr);
+
+    utils_rhomb_array_free(&rarr);
 
     utils_end_log(); 
 
--- arrayutils/src/rhombarray.c
+++ arrayutils/src/rhombarray.c
@@ -1,4 +1,3 @@
-
 #include "rhombarray.h"
 
 #include <stdio.h>
@@ -7,21 +6,25 @@
 #include "logutils.h"
 #include "assertutils.h"
 
-_utils_is_odd(size_t x);
+int _utils_is_odd(size_t x);
+void _putnchar(char c, size_t n, FILE* stream);
 
 static arr_data_t* _utils_rhomb_array_get_ptr(rhomb_array_t* arr, size_t iy, size_t ix);
+static size_t _utils_rhomb_get_row_len(rhomb_array_t* arr, size_t iy);
 
 size_t utils_rhomb_array_get_size(rhomb_array_t* arr)
 {
-    utils_assert(_utils_is_odd(arr->height));
-
     size_t half_height = arr->height / 2ul;
-    return 2 * half_height * half_height + arr->height;
+
+    if(_utils_is_odd(arr->height))
+        return 2 * half_height * half_height + arr->height;
+    else
+        return 2 * half_height * half_height;
 }
 
 enum array_err_t utils_rhomb_array_init(rhomb_array_t* arr, size_t height)
 {
-    utils_assert(_utils_is_odd(arr->height));
+    utils_assert(arr != NULL);
 
     arr->height = height;
     size_t arr_size = utils_rhomb_array_get_size(arr);
@@ -69,27 +72,25 @@ void utils_rhomb_array_free(rhomb_array_t* arr)
     arr->ptr = NULL;
 }
 
-// FIXME
 static arr_data_t *_utils_rhomb_array_get_ptr(rhomb_array_t *arr, size_t iy, size_t ix)
 {
     utils_assert(arr != NULL);
     utils_assert(arr->ptr != NULL);
 
-    if(ix >= 2ul * iy + 1ul) {
+    if(ix >= _utils_rhomb_get_row_len(arr, iy) || iy >= arr->height) {
         utils_log(LOG_LEVEL_ERR, "index out of range");
         return NULL;
     }
 
-    size_t size = utils_rhomb_array_get_size(arr);
     size_t half_height = arr->height / 2ul;
-    size_t offset = 0ul;
+    size_t is_odd      = (size_t)_utils_is_odd(arr->height);
+    size_t offset      = 0ul;
     if(iy < half_height)
         offset = ix + iy * iy;
-
-    if(offset >= size) {
-        utils_log(LOG_LEVEL_ERR, "index out of range");
-        return NULL;
-    }
+    else
+        offset = ix + half_height            * half_height 
+                    + (half_height + is_odd) * (half_height + is_odd)
+                    - (arr->height - iy)     * (arr->height - iy);
 
     return (arr_data_t*)((size_t)arr->ptr + offset * sizeof(arr_data_t));
 }
@@ -98,16 +99,35 @@ void utils_rhomb_array_print(rhomb_array_t* arr)
 {
     utils_assert(arr != NULL);
     utils_assert(arr->ptr != NULL);
-    
+
     for(size_t iy = 0; iy < arr->height; ++iy) {
-        for(size_t ix = 0; ix <= iy; ++ix) 
-            printf("[%lu][%lu] = %d\t", iy, ix, *_utils_rhomb_array_get_ptr(arr, iy, ix));
+        size_t row_size = _utils_rhomb_get_row_len(arr, iy);
+        _putnchar('\t', (arr->height - row_size) / 2ul, stdout);
+
+        for(size_t ix = 0ul; ix < row_size; ++ix)
+            printf("%d\t", iy, ix, *_utils_rhomb_array_get_ptr(arr, iy, ix));
 
         putc('\n', stdout);
     }
 }
 
-_utils_is_odd(size_t x)
+size_t _utils_rhomb_get_row_len(rhomb_array_t* arr, size_t iy)
+{
+    size_t half_height = arr->height / 2ul;
+
+    if(iy < half_height)
+        return 2ul * iy + 1ul;
+    else
+        return 2ul * (arr->height - iy) - 1ul;
+}
+
+int _utils_is_odd(size_t x)
 {
     return x % 2ul == 1ul;
+}
+
+void _putnchar(char c, size_t n, FILE* stream)
+{
+    for(size_t i = 0; i < n; ++i)
+        putc(c, stream);
 }
